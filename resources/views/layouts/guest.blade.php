14d9964e84cece8840f8b3ac6974e7222a2bd443
Kilo <kilo@example.com>
2026-07-06 22:15:24 +0700

    Reduce login card horizontal padding: px-8 -> px-6

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div class="w-full sm:max-w-md mt-6 px-8 py-6 bg-white shadow-sm border border-gray-200 overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-6 bg-white shadow-sm border border-gray-200 overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
